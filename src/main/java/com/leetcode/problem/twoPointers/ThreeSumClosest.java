a55973730d2548722dcf35c064cd4a1e42d46863
Durgaprasad-Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2021-04-06 10:53:09 +0530

    16. 3Sum Closest

--- src/main/java/com/leetcode/problem/twoPointers/ThreeSumClosest.java
+++ src/main/java/com/leetcode/problem/twoPointers/ThreeSumClosest.java
@@ -1,4 +1,36 @@
 package com.leetcode.problem.twoPointers;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int minDiff = Integer.MAX_VALUE;
+        int minSum = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int lo = i + 1;
+            int hi = nums.length - 1;
+
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (sum < target) {
+                    lo++;
+                } else {
+                    hi--;
+                }
+                if(Math.abs(target-sum) < minDiff){
+                    minDiff = Math.abs(target-sum);
+                    minSum = sum;
+                }
+            }
+        }
+        return minSum;
+    }
+
+    public static void main(String[] args){
+        ThreeSumClosest closest = new ThreeSumClosest();
+        int[] nums = {1, -3, -8, 4, 3};
+        System.out.println(closest.threeSumClosest(nums, 1));
+    }
 }
